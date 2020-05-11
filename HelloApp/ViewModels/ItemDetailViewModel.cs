c65e47120266a04286e015cbfefd4bf81d671343
Serares <jianu.rares30@gmail.com>
2020-05-11 22:38:07 +0300

    chore: Files

---
+++ HelloApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HelloApp.Models;
+
+namespace HelloApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
