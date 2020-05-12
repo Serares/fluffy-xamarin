257907057d6edd2a947338ce21aaaaae45957f94
Serares <jianu.rares30@gmail.com>
2020-05-12 11:57:57 +0300

    chore: Relative Layout Files

--- HelloApp/RelativePage.xaml.cs
+++ HelloApp/RelativePage.xaml.cs
@@ -15,6 +15,18 @@ namespace HelloApp
         public RelativePage()
         {
             InitializeComponent();
+            var layout = new RelativeLayout();
+            Content = layout;
+
+            var aquaBox = new BoxView { Color = Color.Aqua };
+            layout.Children.Add(aquaBox,
+                widthConstraint: Constraint.RelativeToParent(parent => parent.Width),
+                heightConstraint: Constraint.RelativeToParent(parent => parent.Height * 0.3));
+            var silverBox = new BoxView { Color = Color.Silver };
+            layout.Children.Add(silverBox,
+                yConstraint: Constraint.RelativeToView(aquaBox, (RelativeLayout, theAquaBox) => theAquaBox.Height + 20)
+                );
+
         }
     }
 }
